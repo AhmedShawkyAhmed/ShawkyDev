a4dd17e76e7d8128cd1a1c067196b9424fff1ad8
Ahmed Shawky <shawkyahmed392@gmail.com>
2025-12-27 12:56:01 +0000

    Failed to construct 'URL': Invalid URL
    i want to display the avatar ima

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,84 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
 
-const repoName = 'studio';
+// const repoName = 'studio';
+
+// const nextConfig: NextConfig = {
+//   output: 'export',
+
+//   basePath: `/${repoName}`,
+//   assetPrefix: `/${repoName}/`,
+
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+
+//   images: {
+//     unoptimized: true, // REQUIRED for GitHub Pages
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'placehold.co',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'picsum.photos',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'drive.google.com',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   images: {
-    unoptimized: true, // REQUIRED for GitHub Pages
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'drive.google.com',
+        port: '',
         pathname: '/**',
       },
     ],
